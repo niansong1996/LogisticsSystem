0833a3ebcf3710bd82c5f8788f74be03d2590a83
Anthony Ni <niansong1996@gmail.com>
2015-10-24 20:51:53 +0800

    timmoc

--- LogisticManageSystem/src/main/java/edu/nju/lms/PO/CommodityPO.java
+++ LogisticManageSystem/src/main/java/edu/nju/lms/PO/CommodityPO.java
@@ -7,6 +7,75 @@ public class CommodityPO {
 	// commodity number
 	private long expressNum;
 	//sender list
-	private SendPO sender;
+	private SendPO send;
+	private LoadPO load;
+	private ArrivalPO arrival;
+	private DispatchPO dispatch;
+	private CheckinPO checkin;
+	private CheckoutPO checkout;
+	private ReceivePo Receive;
+	
+	public CommodityPO(SendPO send){
+		this.expressNum = send.get();
+		this.send = send;
+	}
+
+	public SendPO getSend() {
+		return send;
+	}
+
+	public void setSend(SendPO send) {
+		this.send = send;
+	}
+
+	public LoadPO getLoad() {
+		return load;
+	}
+
+	public void setLoad(LoadPO load) {
+		this.load = load;
+	}
+
+	public ArrivalPO getArrival() {
+		return arrival;
+	}
+
+	public void setArrival(ArrivalPO arrival) {
+		this.arrival = arrival;
+	}
+
+	public DispatchPO getDispatch() {
+		return dispatch;
+	}
+
+	public void setDispatch(DispatchPO dispatch) {
+		this.dispatch = dispatch;
+	}
+
+	public CheckinPO getCheckin() {
+		return checkin;
+	}
+
+	public void setCheckin(CheckinPO checkin) {
+		this.checkin = checkin;
+	}
+
+	public CheckoutPO getCheckout() {
+		return checkout;
+	}
+
+	public void setCheckout(CheckoutPO checkout) {
+		this.checkout = checkout;
+	}
+
+	public ReceivePo getReceive() {
+		return Receive;
+	}
+
+	public void setReceive(ReceivePo receive) {
+		Receive = receive;
+	}
+	
+	
 
 }
