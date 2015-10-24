70a9a53848274fd9682fac630aca330fd26bb883
杜天蛟 <457391179@qq.com>
2015-10-24 22:45:41 +0800

    list vo

--- LogisticManageSystem/src/main/java/edu/nju/lms/VO/ListVO.java
+++ LogisticManageSystem/src/main/java/edu/nju/lms/VO/ListVO.java
@@ -1,6 +1,7 @@
 package edu.nju.lms.VO;
 
 import edu.nju.lms.data.ListState;
+import edu.nju.lms.data.ListType;
 
 /**
  *@author tj
@@ -9,11 +10,12 @@ import edu.nju.lms.data.ListState;
 public class ListVO {
 	protected long id;
 	protected ListState state;
-	
-	public ListVO(long id) {
+	protected ListType type;
+	public ListVO(long id,ListType type) {
 		super();
 		this.id = id;
 		this.state = ListState.WAITING;
+		this.type = type;
 	}
 	public long getId() {
 		return id;
@@ -27,4 +29,11 @@ public class ListVO {
 	public void setState(ListState state) {
 		this.state = state;
 	}
+	public ListType getType() {
+		return type;
+	}
+	public void setType(ListType type) {
+		this.type = type;
+	}
+	
 }
