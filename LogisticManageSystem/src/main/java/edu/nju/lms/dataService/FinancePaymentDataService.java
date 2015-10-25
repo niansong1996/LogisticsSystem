225c50ddff82078e980e2beec31f29bedfc1de3c
oppalu <604597230@qq.com>
2015-10-25 13:44:19 +0800

    .

---
+++ LogisticManageSystem/src/main/java/edu/nju/lms/dataService/FinancePaymentDataService.java
@@ -0,0 +1,54 @@
+package edu.nju.lms.dataService;
+
+import java.rmi.RemoteException;
+import java.util.Calendar;
+import java.util.List;
+
+import edu.nju.lms.PO.EarningsPO;
+import edu.nju.lms.PO.PaymentPO;
+import edu.nju.lms.data.ResultMessage;
+
+/**
+ * the data of the payment
+ * @author oppa
+ * 2015/10/25
+ */
+public interface FinancePaymentDataService {
+	/**
+	 * @param payment
+	 * @return ResultMessage
+	 * @throws RemoteException
+	 */
+	public ResultMessage addPayment(PaymentPO payment) throws RemoteException;
+	
+	/**
+	 * 
+	 * @param date
+	 * @return a list of PaymentPO
+	 * @throws RemoteException
+	 */
+	public List<PaymentPO> findPayment(Calendar date) throws RemoteException;
+	
+	/**
+	 * @param id
+	 * @return ResultMessage
+	 * @throws RemoteException
+	 */
+	public ResultMessage deletePayment(long id) throws RemoteException;
+	
+	/**
+	 * @param Payment
+	 * @return ResultMessage
+	 * @throws RemoteException
+	 */
+	public ResultMessage updatePayment(PaymentPO Payment) throws RemoteException;
+	
+	/**
+	 * show the earnings
+	 * @param date
+	 * @return a list of EarningsPO
+	 * @throws RemoteException
+	 */
+	public List<EarningsPO> findEarning(Calendar date) throws RemoteException;
+	
+}
