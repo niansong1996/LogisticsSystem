1bdea381ce5a564d6aedbd88793cd96a4d9a098f
cuiods <chao14@software.nju.edu.cn>
2015-10-25 20:34:25 +0800

    建包

---
+++ LogisticManageSystem/src/main/java/edu/nju/lms/driver/DepartmentblService_Driver.java
@@ -0,0 +1,14 @@
+package edu.nju.lms.driver;
+
+import edu.nju.lms.businessLogicService.DepartmentblService;
+
+public class DepartmentblService_Driver {
+	public void drive(DepartmentblService service) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
