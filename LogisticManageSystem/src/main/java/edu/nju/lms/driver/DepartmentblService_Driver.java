5e09016feb667abf13a5ff5bd8cd57487231f1ac
cuiods <chao14@software.nju.edu.cn>
2015-10-25 22:53:35 +0800

    department，transport的驱动

--- LogisticManageSystem/src/main/java/edu/nju/lms/driver/DepartmentblService_Driver.java
+++ LogisticManageSystem/src/main/java/edu/nju/lms/driver/DepartmentblService_Driver.java
@@ -1,14 +1,28 @@
 package edu.nju.lms.driver;
 
+import edu.nju.lms.VO.DepartmentVO;
 import edu.nju.lms.businessLogicService.DepartmentblService;
+import edu.nju.lms.businessLogicService.impl.DepartmentblImpl;
+import edu.nju.lms.data.ResultMessage;
 
 public class DepartmentblService_Driver {
 	public void drive(DepartmentblService service) {
-		
+		DepartmentVO department = service.getDepartInfo("0001000");
+		System.out.println(department.getType());
+		ResultMessage result1 = service.addCity(service.findCity("0001000"));
+		System.out.println(result1.isSuccess());
+		ResultMessage result2 = service.deleteDepartment("0000000");
+		System.out.println(result2.isSuccess());
+		ResultMessage result3 = service.updateDepartment(department);
+		System.out.println(result3.isSuccess());
+		ResultMessage result4 = service.addDepartment(department);
+		System.out.println(result4.isSuccess());
 	}
 	
 	public static void main(String[] args) {
-		
+		DepartmentblService departmentService = new DepartmentblImpl();
+		DepartmentblService_Driver driver = new DepartmentblService_Driver();
+		driver.drive(departmentService);
 	}
 
 }
---
+++ LogisticManageSystem/src/main/java/edu/nju/lms/driver/TransportblService_Driver.java
@@ -0,0 +1,78 @@
+package edu.nju.lms.driver;
+
+import java.util.ArrayList;
+
+import edu.nju.lms.VO.ArrivalVO;
+import edu.nju.lms.VO.DispatchVO;
+import edu.nju.lms.VO.LoadVO;
+import edu.nju.lms.VO.OrderInforVO;
+import edu.nju.lms.VO.SendVO;
+import edu.nju.lms.VO.VehicleVO;
+import edu.nju.lms.businessLogicService.TransManageblService;
+import edu.nju.lms.businessLogicService.TransProcessblService;
+import edu.nju.lms.businessLogicService.impl.TransManageblImply;
+import edu.nju.lms.businessLogicService.impl.TransProcessblImpl;
+import edu.nju.lms.data.ArrivalState;
+import edu.nju.lms.data.ResultMessage;
+
+public class TransportblService_Driver {
+	public void drive(TransProcessblService process, TransManageblService manager) {
+		
+		OrderInforVO order = process.checkOrderInfor("0000000000");
+		System.out.println("checkOrderInfor: "+order.getExpressNum());
+		
+		ArrayList<String> baseInfo = new ArrayList<String>();
+		baseInfo.add("张三");
+		SendVO send = process.createSendList(new SendVO("0000000000", "0000000000", baseInfo, 1, 1, "goods", 23, 1));
+		System.out.println("createSendList:"+send.getPrice());
+		
+		ResultMessage result1 = process.saveSendList(send);
+		System.out.println("saveSendList:"+result1.isSuccess());
+		
+		ArrayList<String> commodityNums = new ArrayList<String>();
+		commodityNums.add("0000000000");
+		commodityNums.add("0000000001");
+		LoadVO load = process.createLoadList(new LoadVO("0000000000", "2015/01/02", "0250101", "0000000000", "0250102", "010101", "01",commodityNums , 23));
+		System.out.println("createLoadList:"+load.getDriverNum());
+		
+		ResultMessage result2 = process.saveLoadList(load);
+		System.out.println("saveLoadList:"+result2.isSuccess());
+		
+		ArrivalVO arrival = process.createArrivalList(ArrivalState.COMPLETE, "0000000000");
+		System.out.println("createArrivalList:"+arrival.getDestination());
+		
+		ResultMessage result3 = process.saveArrivalList(arrival);
+		System.out.println("saveArrivalList:"+result3.isSuccess());
+		
+		DispatchVO dispatch = process.createDispatchList("010101");
+		System.out.println("createDispatchList:"+dispatch.getDispatchPerson());
+		
+		ResultMessage result4 = process.saveDispatchList(dispatch);
+		System.out.println("saveDispatchList:"+result4.isSuccess());
+		
+		ResultMessage result5 = process.saveTransportList(null);
+		System.out.println("saveTransportList:"+result5.isSuccess());
+		
+		VehicleVO vehicle = manager.addVehicle(manager.findVehicle("0000000"));
+		System.out.println("addVehicle:"+vehicle.getPlateNum());
+		
+		ResultMessage result6 = manager.saveVehicleInfor(vehicle);
+		System.out.println("saveVehicleInfor:"+result6.isSuccess());
+		
+		ResultMessage result7 = manager.deleteVehicle("00000000");
+		System.out.println("deleteVehicle:"+result7.isSuccess());
+		
+		ResultMessage result8 = manager.updateVehicle(vehicle);
+		System.out.println("updateVehicle:"+result8.isSuccess());
+		
+	}
+	
+	public static void main(String[] args) {
+		TransManageblService manager = new TransManageblImply();
+		TransProcessblImpl process = new TransProcessblImpl();
+		TransportblService_Driver driver = new TransportblService_Driver();
+		driver.drive(process, manager);
+	}
+	
+	
+}
