995db34b71164dae5f45797d4cdfd279cc9bf926
Anthony Ni <niansong1996@gmail.com>
2015-10-26 09:58:16 +0800

    shuju

--- LogisticManageSystem/src/main/java/edu/nju/lms/dataService/impl/UserDataImpl.java
+++ LogisticManageSystem/src/main/java/edu/nju/lms/dataService/impl/UserDataImpl.java
@@ -1,33 +1,67 @@
 package edu.nju.lms.dataService.impl;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import edu.nju.lms.PO.UserPO;
-import edu.nju.lms.data.PersonType;
 import edu.nju.lms.data.ResultMessage;
 import edu.nju.lms.dataService.UserDataService;
 
 public class UserDataImpl implements UserDataService{
+	ArrayList<UserPO> userList = new ArrayList<UserPO>();
+
+	public UserDataImpl(){
+		//TODO Read Serialized-files
+	}
 
 	public ResultMessage addUser(UserPO user) throws RemoteException {
 		// TODO Auto-generated method stub
-		
-		return new ResultMessage(true,null);
+		if(findUser(user.getUserName()).equals(null)){
+			this.userList.add(user);
+			return new ResultMessage(true,null);
+		}
+		else{
+			return new ResultMessage(false,"The user already exists!");
+		}
 	}
 
 	public UserPO findUser(String id) throws RemoteException {
 		// TODO Auto-generated method stub
-		return new UserPO("GaoLu", "123456", PersonType.COURIER);
+		UserPO result = null;
+		Iterator<UserPO> it = userList.iterator();
+		while(it.hasNext()){
+			UserPO next = it.next();
+			if(next.getUserName()==id){
+				result = next;
+				break;
+			}
+		}
+		return result;
 	}
 
 	public ResultMessage deleteUser(String id) throws RemoteException {
 		// TODO Auto-generated method stub
-		return new ResultMessage(true,null);
+		UserPO user = findUser(id);
+		if(!user.equals(null)){
+			userList.remove(user);
+			return new ResultMessage(true,null);
+		}
+		else{
+			return new ResultMessage(false,"Could not find the user!");
+		}
 	}
 
 	public ResultMessage updateUser(UserPO user) throws RemoteException {
 		// TODO Auto-generated method stub
-		return new ResultMessage(true,null);
+		UserPO tempUser = findUser(user.getUserName());
+		if(!tempUser.equals(null)){
+			tempUser = user;
+			return new ResultMessage(true,null);
+		}
+		else{
+			return new ResultMessage(false,"Could not find the user!");
+		}
 	}
 
 }
