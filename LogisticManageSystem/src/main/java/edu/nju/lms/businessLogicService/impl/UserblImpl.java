516ac132e1d39668096a6007249ae297ecfe738e
cuiods <chao14@software.nju.edu.cn>
2015-10-26 09:34:39 +0800

    用户管理的逻辑层

--- LogisticManageSystem/src/main/java/edu/nju/lms/businessLogicService/impl/UserblImpl.java
+++ LogisticManageSystem/src/main/java/edu/nju/lms/businessLogicService/impl/UserblImpl.java
@@ -1,25 +1,70 @@
 package edu.nju.lms.businessLogicService.impl;
 
+import java.rmi.RemoteException;
+
+import edu.nju.lms.PO.UserPO;
 import edu.nju.lms.VO.UserVO;
 import edu.nju.lms.businessLogicService.UserblService;
 import edu.nju.lms.data.ResultMessage;
+import edu.nju.lms.dataService.UserDataService;
 
 public class UserblImpl implements UserblService{
+	
+	private UserDataService dataService = null;
+	
+	public UserblImpl(UserDataService service) {
+		setDataService(service);
+	}
 
 	public UserVO findUserInfo(String id) {
-		return new UserVO("010101", "123456", "总经理");
+		UserPO userPO = null;
+		try {
+			userPO = dataService.findUser(id);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		UserVO user = new UserVO(userPO.getUserName(), userPO.getPassword(), userPO.getPower());
+		return user;
 	}
 
 	public ResultMessage deleteUser(String id) {
-		return new ResultMessage(true, "");
+		ResultMessage result = new ResultMessage(false, "网络未连接");
+		try {
+			result = dataService.deleteUser(id);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
-	public ResultMessage updateUser(UserVO User) {
-		return new ResultMessage(true, "");
+	public ResultMessage updateUser(UserVO user) {
+		ResultMessage result = new ResultMessage(false, "网络未连接");
+		UserPO userPO = new UserPO(user.getUserName(), user.getPassword(), user.getPower());
+		try {
+			result = dataService.updateUser(userPO);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	public ResultMessage addUser(UserVO User) {
-		return new ResultMessage(true, "");
+		ResultMessage result = new ResultMessage(false, "网络未连接");
+		UserPO userPO = new UserPO(User.getUserName(), User.getPassword(), User.getPower());
+		try {
+			result = dataService.addUser(userPO);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public UserDataService getDataService() {
+		return dataService;
+	}
+
+	public void setDataService(UserDataService dataService) {
+		this.dataService = dataService;
 	}
 
 }
