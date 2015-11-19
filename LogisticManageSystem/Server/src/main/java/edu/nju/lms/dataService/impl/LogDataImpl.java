eb44b94b05ab6057cdb07e73db17dbffb09b37c2
oppalu <604597230@qq.com>
2015-11-19 19:58:03 +0800

    log写完了，先传一下
    在server那边测试过了

--- LogisticManageSystem/Server/src/main/java/edu/nju/lms/dataService/impl/LogDataImpl.java
+++ LogisticManageSystem/Server/src/main/java/edu/nju/lms/dataService/impl/LogDataImpl.java
@@ -1,36 +1,75 @@
 package edu.nju.lms.dataService.impl;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Date;
 
 import edu.nju.lms.PO.OperationPO;
 import edu.nju.lms.data.ResultMessage;
 import edu.nju.lms.dataService.LogDataService;
 /**
  * @author oppalu
- * 2015/11/18
+ * @date 2015/11/19
  *
  */
 public class LogDataImpl implements LogDataService{
-	
-	private ArrayList<OperationPO> operationList =new ArrayList<OperationPO>();
 
+	/**
+	 * add a log info
+	 * write it into the txt file
+	 */
 	public ResultMessage addOperation(OperationPO operation) throws RemoteException {
-		operationList.add(operation);
+		try {
+			File file=new File("log.txt");
+			if(!file.exists()){
+				file.createNewFile();
+			}
+			FileWriter fw=new FileWriter(file,true);
+			
+			fw.write(operation.getTime()+" "+operation.getUser()+" "+operation.getExplain());
+			fw.write("\r\n");
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return new ResultMessage(true,null);
 	}
 
-	public List<OperationPO> findOperation(Calendar begin, Calendar end) throws RemoteException {
-		Iterator<OperationPO> it=operationList.iterator();
+	/**
+	 * find the info of log in valid time
+	 */
+	public ArrayList<OperationPO> findOperation(Calendar begin, Calendar end) throws RemoteException {
 		ArrayList<OperationPO> result=new ArrayList<OperationPO>();
-		while(it.hasNext()){
-			OperationPO operation=it.next();
-			if(begin.before(operation.getTime())&&end.after(operation.getTime())){
-				result.add(operation);
+		String get="";
+		OperationPO op;
+		String[] temp;
+		Calendar c=Calendar.getInstance();
+		try {
+			File file=new File("log.txt");
+			FileReader fr=new FileReader(file);
+			BufferedReader br=new BufferedReader(fr);
+			while((get=br.readLine())!=null){
+				temp=get.split(" ");
+				String[] temp1=temp[0].split("/");
+				int year=Integer.parseInt(temp1[0]);
+				int month=Integer.parseInt(temp1[1]);
+				int day=Integer.parseInt(temp1[2]);
+				c.set(year, month, day);
+				if(begin.before(c)&&end.after(c)){
+					op=new OperationPO(temp[0],temp[1],temp[2]);
+					result.add(op);
+				}
 			}
+			br.close();
+			fr.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return result;
 	}
