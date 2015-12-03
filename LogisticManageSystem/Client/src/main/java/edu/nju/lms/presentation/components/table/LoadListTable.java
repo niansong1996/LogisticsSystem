92acc1943c9a80fb5c6e0078cbb58878726395d2
cuiods <chao14@software.nju.edu.cn>
2015-12-03 23:14:56 +0800

    传一下

---
+++ LogisticManageSystem/Client/src/main/java/edu/nju/lms/presentation/components/table/LoadListTable.java
@@ -0,0 +1,19 @@
+package edu.nju.lms.presentation.components.table;
+
+import org.dom4j.Element;
+
+import edu.nju.lms.presentation.UIController;
+
+public class LoadListTable extends MyTable{
+
+	private static final long serialVersionUID = 1380293244986696457L;
+
+	public LoadListTable(Element element, UIController controller) {
+		super(element, controller);
+	}
+
+	@Override
+	protected void getData() {
+		
+	}
+}
