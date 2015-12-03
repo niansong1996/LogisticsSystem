a73ceb0a6c3e79aae75e2e7a4dd4aaa908e2409d
Anthony Ni <niansong1996@gmail.com>
2015-12-03 21:16:56 +0800

    commit

---
+++ LogisticManageSystem/Client/src/main/java/edu/nju/lms/businessLogicService/impl/list/ListNumOccupancy.java
@@ -0,0 +1,7 @@
+package edu.nju.lms.businessLogicService.impl.list;
+
+public class ListNumOccupancy {
+	public static NumOccupancyPO getNumOccupancy(){
+		
+	}
+}
---
+++ LogisticManageSystem/Common/src/main/java/edu/nju/lms/PO/NumOccupancyPO.java
@@ -0,0 +1,8 @@
+package edu.nju.lms.PO;
+
+public class NumOccupancyPO {
+	String SendListNum;
+	public NumOccupancyPO(String SendListNum){
+		this.SendListNum = SendListNum;
+	}
+}
