5a60e46f2c5d38f803202da66dc9a3ec214fa07c
杜天蛟 <457391179@qq.com>
2015-12-08 19:28:23 +0800

    登陆

--- LogisticManageSystem/Client/src/main/java/edu/nju/lms/presentation/mouseListener/SignInButtonListener.java
+++ LogisticManageSystem/Client/src/main/java/edu/nju/lms/presentation/mouseListener/SignInButtonListener.java
@@ -29,49 +29,47 @@ public class SignInButtonListener extends ButtonListener {
 	public void mouseReleased(MouseEvent e) {
 		JTextField n = (JTextField) units.get(1);
 		JTextField p = (JTextField) units.get(2);
-		String name = "";
-		name = n.getText();
-		String password = "";
-		password = p.getText();
+		String name = n.getText();
+		String password = p.getText();
+		if(name.isEmpty()||password.isEmpty()){
+			return;
+		}
 		n.setText("");
 		p.setText("");
-		controller.changeTo("warehousePanel");
-//		controller.initialize(PersonType.WAREHOUSE);
-//		if (name != "" && password != "") {
-//			if(userController==null){
-//				return;
-//			}
-//			UserVO info = userController.findUserInfo(name);
-//			System.out.println("password"+info.getPassword());
-//			System.out.println(password);
-//			if (info.getUserName() == null) {
-//				MyDialog error = new MyDialog("notExist");
-//			} else if (info.getPassword().equals(password)) {
-//				controller.setLogID(name);
-//				controller.initialize(info.getPower());
-//				switch (info.getPower()) {
-//				case MANAGER:
-//					controller.changeTo("ManagerPanel");
-//					break;
-//				case FINANCIAL_ADVANCED:
-//					break;
-//				case FINANCIAL_NORMAL:
-//					break;
-//				case ADMINISTRATOR:
-//					break;
-//				case COUNTER_INTERMEDIATE:
-//					break;
-//				case COUNTER_BUSSINESS:
-//					break;
-//				case WAREHOUSE:
-//					break;
-//				case COURIER:
-//					break;
-//
-//				}
-//			} else {
-//				MyDialog error = new MyDialog("passwordError");
-//			}
-//		}
+		UserVO info = userController.findUserInfo(name);
+		if (info.getUserName() == null) {
+			MyDialog error = new MyDialog("notExist");
+		} else if (info.getPassword().equals(password)) {
+			controller.setLogID(name);
+			switch (info.getPower()) {
+			case MANAGER:
+				controller.changeTo("ManagerPanel");
+				break;
+			case FINANCIAL_ADVANCED:
+				controller.changeTo("FinancialAdvancedPanel");
+				break;
+			case FINANCIAL_NORMAL:
+				controller.changeTo("FinancialCommonPanel");
+				break;
+			case ADMINISTRATOR:
+				controller.changeTo("AdministratorPanel");
+				break;
+			case COUNTER_INTERMEDIATE:
+				controller.changeTo("counterInterPanel");
+				break;
+			case COUNTER_BUSSINESS:
+				controller.changeTo("counterBussinessPanel");
+				break;
+			case WAREHOUSE:
+				controller.changeTo("warehousePanel");
+				break;
+			case COURIER:
+				controller.changeTo("courierPanel");
+				break;
+
+			}
+		} else {
+			MyDialog error = new MyDialog("passwordError");
+		}
 	}
 }
