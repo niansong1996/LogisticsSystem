0d43a32e559fc3032ac5776dda13f1b9acc153eb
Anthony Ni <niansong1996@gmail.com>
2015-12-09 16:13:42 +0800

    commit

---
+++ LogisticManageSystem/Client/src/test/java/simpletest/ClassInfo.java
@@ -0,0 +1,24 @@
+package simpletest;
+
+public class ClassInfo {
+double complexity;
+int lineOfCode;
+int lineOfExlanation;
+double CBO;
+double DAC;
+double NOC;
+double DIT;
+double LCOM;
+public ClassInfo(double complexity, int lineOfCode, int lineOfExlanation, double cBO, double dAC, double nOC,
+		double dIT, double lCOM) {
+	this.complexity = complexity;
+	this.lineOfCode = lineOfCode;
+	this.lineOfExlanation = lineOfExlanation;
+	CBO = cBO;
+	DAC = dAC;
+	NOC = nOC;
+	DIT = dIT;
+	LCOM = lCOM;
+}
+
+}
