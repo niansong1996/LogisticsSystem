26b774f86544a63e24759c08f806d2db01de1538
Anthony Ni <niansong1996@gmail.com>
2015-12-09 16:15:46 +0800

    commit

--- LogisticManageSystem/Client/src/test/java/simpletest/ClassInfo.java
+++ LogisticManageSystem/Client/src/test/java/simpletest/ClassInfo.java
@@ -1,24 +1,27 @@
 package simpletest;
 
 public class ClassInfo {
-double complexity;
-int lineOfCode;
-int lineOfExlanation;
-double CBO;
-double DAC;
-double NOC;
-double DIT;
-double LCOM;
-public ClassInfo(double complexity, int lineOfCode, int lineOfExlanation, double cBO, double dAC, double nOC,
-		double dIT, double lCOM) {
-	this.complexity = complexity;
-	this.lineOfCode = lineOfCode;
-	this.lineOfExlanation = lineOfExlanation;
-	CBO = cBO;
-	DAC = dAC;
-	NOC = nOC;
-	DIT = dIT;
-	LCOM = lCOM;
-}
+	String name;
+	double complexity;
+	int lineOfCode;
+	int lineOfExlanation;
+	double CBO;
+	double DAC;
+	double NOC;
+	double DIT;
+	double LCOM;
+	public ClassInfo(String name, double complexity, int lineOfCode, int lineOfExlanation, double cBO, double dAC,
+			double nOC, double dIT, double lCOM) {
+		this.name = name;
+		this.complexity = complexity;
+		this.lineOfCode = lineOfCode;
+		this.lineOfExlanation = lineOfExlanation;
+		CBO = cBO;
+		DAC = dAC;
+		NOC = nOC;
+		DIT = dIT;
+		LCOM = lCOM;
+	}
+
 
 }
