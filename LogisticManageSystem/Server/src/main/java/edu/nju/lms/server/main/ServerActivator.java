0f2f4b2bdac0d7fe5d812ca3346c058f8ff5d59d
Anthony Ni <niansong1996@gmail.com>
2015-12-14 11:10:40 +0800

    服务器界面写完了，添加了设置和停止

--- LogisticManageSystem/Server/src/main/java/edu/nju/lms/server/main/ServerActivator.java
+++ LogisticManageSystem/Server/src/main/java/edu/nju/lms/server/main/ServerActivator.java
@@ -2,6 +2,12 @@ package edu.nju.lms.server.main;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.ExportException;
@@ -45,30 +51,26 @@ import edu.nju.lms.dataService.impl.WarehouseCheckinDataImpl;
 import edu.nju.lms.dataService.impl.WarehouseCheckoutDataImpl;
 import edu.nju.lms.dataService.impl.WarehouseDataImpl;
 
-public class ServerActivator 
-{
+public class ServerActivator {
+	private String userID;
+	private String userPassword;
+	private String dataBaseID;
+	private String dataBasePassword;
+	private String IPAddress;
+	private String port;
+	private boolean firstLogin = false;
+
+	public ServerActivator(){
+		
+	}
+
 	public static void main( String[] args ){
 		ServerActivator activator = new ServerActivator();
-//		try {
-//			UIManager.setLookAndFeel("com.alee.laf.WebLookAndFeel");
-//		} catch (ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (InstantiationException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IllegalAccessException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (UnsupportedLookAndFeelException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 		activator.InitServerUI();
 	}
-	public void activateServer(String port,String user,String password){
-		String url = "jdbc:mysql://127.0.0.1:"+port+"/lms?useUnicode=true&characterEncoding=utf8";
-		JDBC.createJDBC(url,user,password);
+	public void activateServer(){
+		String url = "jdbc:mysql:"+this.IPAddress+":"+port+"/lms?useUnicode=true&characterEncoding=utf8";
+		JDBC.createJDBC(url,this.dataBaseID,this.dataBasePassword);
 
 		try{    
 			LocateRegistry.createRegistry(1099);  
@@ -122,7 +124,8 @@ public class ServerActivator
 		final JFrame frame = new JFrame("服务器启动器");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setSize(300, 120);
-		JPanel panel = new JPanel();
+		frame.setLocationRelativeTo(null);
+		final JPanel panel = new JPanel();
 		frame.getContentPane().add(panel);
 
 		JLabel label1 = new JLabel("用户名");
@@ -140,26 +143,137 @@ public class ServerActivator
 
 		frame.setVisible(true);
 
+		this.readSettings();
+		
 		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String user = text1.getText();
 				String password = text2.getText();
-				if(user.equals("root")&&password.equals("9990"))
-					activateServer("3306",user,password);
-				else{
+				if(user.equals(userID)&&password.equals(userPassword)){
+					activateServer();
+					panel.setVisible(false);
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					initEnterUI(frame);
+				}else{
+					frame.setVisible(false);
 					jumpHint("用户名或密码错误！");
+					InitServerUI();
 				}
-				frame.setVisible(false);
 			}
 		}
 				);	
 	}
-	
+	public void initEnterUI(JFrame parent){
+		JPanel panel = new JPanel();
+		parent.getContentPane().add(panel);
+
+		JLabel hint = new JLabel("服务器正在运行...");
+		JButton settings = new JButton("设置");
+		JButton halt = new JButton("停止服务器运行");
+
+		panel.add(hint);
+		panel.add(settings);
+		panel.add(halt);
+		settings.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				initSettingsUI();
+			}
+		});
+		halt.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+	public void initSettingsUI(){
+		final JFrame frame = new JFrame("设置");
+		frame.setSize(200, 360);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
+
+		JLabel serverID = new JLabel("服务器登陆账号:");
+		JLabel serverpwd = new JLabel("服务器登陆密码:");
+		JLabel dbID = new JLabel("数据库登陆账号:");
+		JLabel dbpwd = new JLabel("数据库密码:");
+		JLabel dbIP = new JLabel("数据库IP");
+		JLabel dbp = new JLabel("数据库端口:");
+
+		final JTextField serverIDtf = new JTextField(15);
+		final JTextField serverpwdtf = new JTextField(15);
+		final JTextField dbIDtf = new JTextField(15);
+		final JTextField dbpwdtf = new JTextField(15);
+		final JTextField dbIPtf = new JTextField(15);
+		final JTextField dbptf = new JTextField(15);
+
+		JButton save = new JButton("保存");
+
+		panel.add(serverID);panel.add(serverIDtf);
+		panel.add(serverpwd);panel.add(serverpwdtf);
+		panel.add(dbID);panel.add(dbIDtf);
+		panel.add(dbpwd);panel.add(dbpwdtf);
+		panel.add(dbIP);panel.add(dbIPtf);
+		panel.add(dbp);panel.add(dbptf);
+
+		panel.add(save);
+
+		frame.setVisible(true);
+
+		save.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				saveSettings(serverIDtf.getText(),serverpwdtf.getText(),dbIDtf.getText(),dbpwdtf.getText()
+						,dbIPtf.getText(),dbptf.getText());
+				readSettings();
+				frame.setVisible(false);
+			}
+
+		});
+	}
+	public void saveSettings(String serverID,String serverPassword,String databaseID,String databasePassword,String databaseIP,String databasePort){
+		File settings = new File("src/main/resources/server_settings.lms");
+		try {
+			FileWriter fw = new FileWriter(settings);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(serverID+"\n");
+			bw.write(serverPassword+"\n");
+			bw.write(databaseID+"\n");
+			bw.write(databasePassword+"\n");
+			bw.write(databaseIP+"\n");
+			bw.write(databasePort+"\n");
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	public void readSettings(){
+		File settings = new File("src/main/resources/server_settings.lms");
+		try {
+			if(!settings.exists()){
+				this.firstLogin = true;
+				settings.createNewFile();
+				this.jumpHint("这是您第一次登陆，请完成相关设置");
+			}
+			FileReader fr = new FileReader(settings);
+			BufferedReader br = new BufferedReader(fr);
+			this.userID = br.readLine();
+			this.userPassword = br.readLine();
+			this.dataBaseID = br.readLine();
+			this.dataBasePassword = br.readLine();
+			this.IPAddress = br.readLine();
+			this.port = br.readLine();
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	public void jumpHint(String msg){
 		final JFrame frame = new JFrame("提示");
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setSize(150, 100);
+		frame.setSize(250, 100);
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel);
 
@@ -174,9 +288,10 @@ public class ServerActivator
 		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame.setVisible(false);
+				if(firstLogin) 	initSettingsUI();
 			}
 		}
 				);	
-		
+
 	}
 }
