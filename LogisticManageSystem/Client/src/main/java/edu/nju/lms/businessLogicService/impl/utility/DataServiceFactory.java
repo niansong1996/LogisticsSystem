900b0f50dcd57136411db7099a47bb35a88fc0a0
oppalu <604597230@qq.com>
2015-12-17 00:01:23 +0800

    加了两个标题

--- LogisticManageSystem/Client/src/main/java/edu/nju/lms/businessLogicService/impl/utility/DataServiceFactory.java
+++ LogisticManageSystem/Client/src/main/java/edu/nju/lms/businessLogicService/impl/utility/DataServiceFactory.java
@@ -22,7 +22,7 @@ import edu.nju.lms.dataService.WarehouseCheckoutDataService;
 import edu.nju.lms.dataService.WarehouseDataService;
 
 public class DataServiceFactory {
-	private static String IPAddress = "114.212.42.136";
+	private static String IPAddress = "127.0.0.1";
 	private static String portNum = "1099";
 	private static Remote getDataService(String dataServiceName){
 		try {
