899fb8a3355a956b7b9b5945926778f7f5473da2
cuiods <chao14@software.nju.edu.cn>
2015-12-23 23:21:53 +0800

    财务人员的bug

--- LogisticManageSystem/Client/src/main/java/edu/nju/lms/presentation/mouseListener/SetTotalRowListener.java
+++ LogisticManageSystem/Client/src/main/java/edu/nju/lms/presentation/mouseListener/SetTotalRowListener.java
@@ -21,7 +21,7 @@ public class SetTotalRowListener extends ButtonListener {
 	public SetTotalRowListener(ArrayList<Component> units, UIController controller, Component button) {
 		super(units, controller, button);
 		this.control = controller.getWarehouseController();
-		this.field = (JTextField) units.get(26);
+		this.field = (JTextField) units.get(13);
 	}
 
 	@Override
