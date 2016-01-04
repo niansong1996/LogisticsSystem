c8158bc3eb4dbca867fc1425935f2278b8be80b9
cuiods <chao14@software.nju.edu.cn>
2016-01-04 21:07:14 +0800

    这里还有一个小bug'

--- LogisticManageSystem/Client/src/main/java/edu/nju/lms/presentation/mouseListener/AddDriverListener.java
+++ LogisticManageSystem/Client/src/main/java/edu/nju/lms/presentation/mouseListener/AddDriverListener.java
@@ -45,7 +45,7 @@ public class AddDriverListener extends ButtonListener{
 		MyComboBox<String> sex = (MyComboBox<String>) units.get(6);
 		Calendar c = date.getCalendar();
 		String str1 = (c.get(Calendar.MONTH)+1)>9?"":"0";
-		String str2 = c.get(Calendar.DATE)<10?"1":"0";
+		String str2 = c.get(Calendar.DATE)<10?"0":"";
 		String birth = c.get(Calendar.YEAR) + "-" +str1+(c.get(Calendar.MONTH) + 1) + "-" +str2+ c.get(Calendar.DATE);
 		DriverVO driver = new DriverVO("", name.getText(), birth, idCard.getText(), phone.getText(),
 				sex.getSelectedIndex(), limit.getText(), businesshall.getText());
